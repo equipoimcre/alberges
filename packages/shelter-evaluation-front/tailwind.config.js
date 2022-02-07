bede495834fcbb600312a12345c70e83b1581f97
sr.teppei <sr.teppei@teppeilabs.com>
2022-02-07 19:30:32 +0100

    Add icons and colors

--- packages/shelter-evaluation-front/tailwind.config.js
+++ packages/shelter-evaluation-front/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   prefix: '',
   purge: {
@@ -7,6 +9,32 @@ module.exports = {
   },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+    colors: {
+      black: {
+        ...colors.black,
+        DEFAULT: '#323232',
+      },
+      gray: {
+        ...colors.gray,
+        DEFAULT: '#ededed',
+      },
+      white: {
+        ...colors.white,
+        DEFAULT: '#ffffff',
+      },
+      red: {
+        ...colors.red,
+        DEFAULT: '#f5333f',
+      },
+      blue: {
+        ...colors.blue,
+        DEFAULT: '#011e41',
+      },
+    },
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      gray: '#ededed',
+    }),
     extend: {},
   },
   variants: {
